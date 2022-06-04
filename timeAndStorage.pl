2bdff8208f99654c7b594ca4fac9765ea67e8375
IvenBu <49495011+IvenBu@users.noreply.github.com>
2022-06-04 21:19:35 +0200

    Update timeAndStorage.pl

--- timeAndStorage.pl
+++ timeAndStorage.pl
@@ -1,39 +1,44 @@
-:- module(timeAndStorage,[time/2,storageBytes/3]).
+:- module(timeAndStorage,[measurement/3]).
+
 :- use_module(datastructures).
 
-/* Zeitmessung */
-time(Time, Prozedur) :-
+/* Time */
+measurement(time,Time,Prozedur):-
         clean,
         statistics(walltime,[Start,_]),
         Prozedur,
+        !,
         statistics(walltime,[Stop,_]),
         Time is Stop - Start,!.
 
-/* Messen der Speicherbelegung */
-storageBytes(heap,Procedure,Bytes) :-
+/* Storage*/
+measurement(heap,Bytes,Procedure) :-
         clean,
         statistics(heap,[Start,_]),
         Procedure,
+        !,
         statistics(heap,[Stop,_]),
-        Bytes is Stop-Start.
+        Bytes is Stop-Start,!.
 
-storageBytes(garbage,Procedure,[NumberGC,Bytes,Time]) :-
+measurement(garbage,[NumberGC,Bytes,Time],Procedure) :-
         clean,
         statistics(garbage_collection,[Start1,Start2,Start3]),
         Procedure,
+        !,
         statistics(garbage_collection,[Stop1,Stop2,Stop3]),
         NumberGC is Stop1 - Start1,
         Bytes is Stop2 - Start2,
         Time is Stop3 - Start3,
         !.
 
-/* Storage:global_stack_used, local_stack_used, trail_used, choice_used, heap ,garbage(Ausgabe is [no. of GCs,bytes freed,time spent]*/
-storageBytes(Storage,Procedure,Bytes) :-
+/* Storage:global_stack_used, local_stack_used, trail_used, choice_used*/
+measurement(Storage,Bytes,Procedure) :-
         clean,
         statistics(Storage,Start),
         Procedure,
+        !,
         statistics(Storage,Stop),
-        Bytes is Stop-Start.
+        Bytes is Stop-Start,!.
 
 
 
