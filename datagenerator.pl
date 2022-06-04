769a21490fae83387dc692232c30a8e4a9f11681
IvenBu <49495011+IvenBu@users.noreply.github.com>
2022-06-04 21:15:23 +0200

    Update datagenerator.pl

--- datagenerator.pl
+++ datagenerator.pl
@@ -1,10 +1,11 @@
-:- module(datagenerator,[data/3, data/4]).
+:- module(datagenerator,[data/3,datagenerator/4,datagenerator/5,datagenerator/4]).
 
 :- use_module(library(sets),[del_element/3]).
 :- use_module(library(between)).
 :- use_module(library(lists)).
 :- use_module(library(file_systems),[file_members_of_directory/3]).
 :- use_module(library(random),[random_member/2]).
+:- use_module(library(random)).
 
 include_type_definition(_-FullPath) :- consult(FullPath).
 
@@ -12,45 +13,49 @@ include_type_definition(_-FullPath) :- consult(FullPath).
    maplist(include_type_definition,FileList). 
 
 /* Erzeugung der Daten unter der Verwendung der generate/2 Funktion aus dem Fuzzer */
-                                
+
+datagenerator(ValueType,KeyType,Size,Keys,Values):-
+	data(KeyType,Size,Keys),
+	data(ValueType,Size,Values),!.
+        	
+datagenerator(random,Keys,Size,Data):-
+	shuffle(Size,Keys,[],Data),!.
+
+datagenerator(last,Keys,Size,Data):-
+	length(Keys,X),
+	Helper is X-Size,
+	sublist(Keys,List,Helper,Size,0),
+	reverse(List, Data),!.
+
+datagenerator(first,Keys,Size,Data):-
+	length(Keys,X),
+	Helper is X-Size,
+	sublist(Keys,Data,0,Size,Helper),!.
+	
+datagenerator(KeyType,ToGet,AccessType,GetKeys):-
+	data(AccessType,KeyType,ToGet,GetKeys),!.
+             
 data(string,X, Data) :-
-        generate(list(list(integer([between(33,126)])),[size:X]),Data).
+        generate(list(list(integer([between(33,126)])),[size:X]),Data),!.
 
 data(atom,X, Data) :-
-        generate(list(atom([alph]),[size:X]),Data).
+        generate(list(atom([alph]),[size:X]),Data),!.
 
 data(ordIdx,X,Data) :-
-        numlist(1,X, Data).
+        numlist(1,X, Data),!.
         
-data(unordIdx,X,Data) :-
-	numlist(1,X,H),
-	shuffle(X,H,[],Data).
-	
-data(idxrev,X,Data) :-
-	numlist(1,X,H),
-	reverse(H,Data).
-
 data(integer,X,Data) :-
-        generate(list(integer([]),[size:X]),Data).
+        generate(list(integer([]),[size:X]),Data),!.
         
-/* Für die random Auswahl von X Integern aus einer Liste Helper*/
-data(Keys,Count, Data) :-
-	shuffle(Count,Keys,[],Data).
+
+/* Für die random Auswahl von X Werten aus einer Liste*/
 
 shuffle(0,_,Values,Values).
 
 shuffle(X,H,Acc,Data) :-
-	random_member(Value,H),
-        SizeNew is X - 1,
-        del_element(Value,H,HNew),
-        shuffle(SizeNew,HNew,[Value|Acc],Data).
-        
-/* für den Zugriff auf die Elemente von Hinten*/  
-data(ordIdx,X,Count,Keys) :-
-	H is (X - (Count - 1)),
-	numlist(H,X,List),
-	reverse(List, Keys).
-	
+	random_select(Value, H, Rest),
+	SizeNew is X - 1,
+	shuffle(SizeNew,Rest,[Value|Acc],Data).
 	
 	
 	
